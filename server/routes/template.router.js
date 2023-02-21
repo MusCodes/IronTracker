76080c92a8d754fcf967b31de3c636949a9b90ba
Mustafa <MustafaGino1@yahoo.com>
2023-02-21 10:53:13 -0600

    Get route for user exercise completed and tested with postman.

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get("/user_exercise", (req, res) => {
+  const QUERYTEXT = `SELECT * FROM "user_exercise";`;
+  pool
+    .query(QUERYTEXT)
+    .then((results) => {
+      res.send(results.rows);
+    })
+    .catch((error) => {
+      console.log("error in GET line 12", error);
+      res.sendStatus(500);
+    });
   // GET route code here
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // POST route code here
 });
 
