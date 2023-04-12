7c242a3bcb6a66d18489dfbdaa4c1f7a13097cbf
Mustafa <MustafaGino1@yahoo.com>
2023-04-12 05:47:16 -0500

    added code comments and formatted sagas
    .

--- src/redux/sagas/exercise.saga.js
+++ src/redux/sagas/exercise.saga.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 import { takeLatest } from "redux-saga/effects";
 
-
+// This generator function handles adding an exercise to the database
 function* addExercise(action) {
-    try {
-     
-      const response = yield axios.post(`/api/exercises/${action.payload.template_id}`, { name: action.payload.name});
-      console.log('THIS IS RESPONSE FOR EXERCISES', response);
-      //yield put({type:"GET_EXERCISE_TABLE"})
-      //yield action.payload.history.push(`/exercises/${response.data}`)
-    } catch (error) {
-      console.log('ERROR ON LINE 18', error);
-    }
-
+  try {
+    // The axios.post() function sends a POST request to the specified URL with the given template ID and exercise name
+    const response = yield axios.post(
+      `/api/exercises/${action.payload.template_id}`,
+      { name: action.payload.name }
+    );
+    console.log("THIS IS RESPONSE FOR EXERCISES", response);
+  } catch (error) {
+    console.log("ERROR ON LINE 18", error);
   }
+}
 
-  function* addExerciseSaga(){
-    yield takeLatest("ADD_EXERCISES", addExercise)
-  }
-  export default addExerciseSaga
+function* addExerciseSaga() {
+  yield takeLatest("ADD_EXERCISES", addExercise);
+}
+export default addExerciseSaga;
