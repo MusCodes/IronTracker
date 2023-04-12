32bbff46aded303d2b914dd95a7d4b96746a70e3
Mustafa <MustafaGino1@yahoo.com>
2023-04-12 03:52:00 -0500

    Once a user registers, it no longer directs them to the about page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -96,7 +96,7 @@ function App() {
             {user.id ? (
               // If the user is already logged in,
               // redirect them to the /user page
-              <Redirect to="/user" />
+              <Redirect to="/test" />
             ) : (
               // Otherwise, show the registration page
               <RegisterPage />
